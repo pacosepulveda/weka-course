48a40fb2cbddd069aea807114260c8ecc3944e01
pacosepulveda <pacosepulveda@gmail.com>
2026-05-04 19:47:04 +0200

    Add script to start desktop environment with Xvfb

---
+++ scripts/start-desktop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+export DISPLAY=:1
+export RESOLUTION=${RESOLUTION:-1600x900x24}
+
+pkill Xvfb || true
+pkill x11vnc || true
+pkill websockify || true
+pkill fluxbox || true
+
+Xvfb :1 -screen 0 ${RESOLUTION} &
+sleep 2
+
+fluxbox &
+sleep 2
+
+x11vnc -display :1 -forever -shared -nopw -rfbport 5901 &
+sleep 2
+
+websockify --web=/usr/share/novnc/ 6080 localhost:5901
