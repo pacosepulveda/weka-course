c789aa29d10451a82f974ff37fea28bf6da0dc7a
pacosepulveda <pacosepulveda@gmail.com>
2026-05-04 21:26:24 +0200

    Configure Weka environment and launch application
    Set up environment variables and directories for Weka.

--- scripts/start-weka.sh
+++ scripts/start-weka.sh
@@ -2,6 +2,17 @@
 set -e
 
 export DISPLAY=:1
-cd /workspaces/weka-codespaces-course
 
-java -Xmx2g -jar /opt/weka/weka.jar
+export WEKA_HOME=/workspaces/weka-course/.weka
+export WEKA_PACKAGE_DIR=/workspaces/weka-course/.weka/packages
+export WEKA_NATIVE_LIBS=/workspaces/weka-course/.weka/native
+export WEKA_REPOSITORY_CACHE=/workspaces/weka-course/.weka/repCache
+
+mkdir -p "$WEKA_PACKAGE_DIR"
+mkdir -p "$WEKA_NATIVE_LIBS"
+mkdir -p "$WEKA_REPOSITORY_CACHE"
+mkdir -p /workspaces/weka-course/student-work
+
+cd /workspaces/weka-course
+
+java -Xmx2g -Duser.home=/workspaces/weka-course -jar /opt/weka/weka.jar
