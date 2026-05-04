815c52968dba4e0e9670e29dac97d7b7d1424247
pacosepulveda <pacosepulveda@gmail.com>
2026-05-04 21:26:02 +0200

    Enhance start-desktop.sh with x11vnc options
    Add additional options to x11vnc command and create fluxbox directory.

--- scripts/start-desktop.sh
+++ scripts/start-desktop.sh
@@ -9,13 +9,23 @@ pkill x11vnc || true
 pkill websockify || true
 pkill fluxbox || true
 
+mkdir -p ~/.fluxbox
+
 Xvfb :1 -screen 0 ${RESOLUTION} &
 sleep 2
 
 fluxbox &
 sleep 2
 
-x11vnc -display :1 -forever -shared -nopw -rfbport 5901 &
-sleep 2
+x11vnc \
+  -display :1 \
+  -forever \
+  -shared \
+  -nopw \
+  -rfbport 5901 \
+  -noxdamage \
+  -repeat \
+  -xkb \
+  -bg
 
 websockify --web=/usr/share/novnc/ 6080 localhost:5901
